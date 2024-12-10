dd228ea5128f2f129127f7b127b24e4ebd536f78
daniela vera <lizethdanielaveralopez@gmail.com>
2024-12-10 00:02:51 -0500

    tipos de datos en javascript

---
+++ tipos-datos.js
@@ -0,0 +1,10 @@
+
+//NUMEROS
+let numeroVariable = 10 // valor numerico
+let decimalVariable = 10.10 // valor flotante
+
+//TEXTO
+let textoVariable = 'texto' //valor string utilizando comillas simples
+let texto2 = "texto" //valor string utilizando comillas dobles
+let texto3 = `este es el valor de la variable ${numeroVariable}` //valor string concatenado
+console.log(texto3)
---
+++ variables.js
@@ -0,0 +1,22 @@
+var nombreDeLaVariable = 'Curso de javascript'
+let nombreDeVariable = 'aprendiendo javascript'
+const nombreVariable = 'con sergie code'
+
+
+let x, y, z;
+x = 5;
+y = 10;
+z = x + y;
+console.log(z);
+
+/**ANOTACIONES
+ * Al definir variables con var y let se permite no asignarles ningun valor pero con const si lo requiere 
+ 
+ * SCOPE: Es el ambiente en el que va a estar disponible el codigo
+  - var : si se declara dentro de una funcion solo es accesible dentro de esa funcion y si se declara fuera es global 
+  - let y const : solo son accesibles dentro de la funcion en la que se declararon
+
+ * En las variables let y var se puede reasignar y en el caso de la variable const no se puede
+
+ * Con let se puede declarar varias variables al mismo tiempo  
+ */
