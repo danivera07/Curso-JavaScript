82bb368aed20ab9f4b0263165b6e0b232780e8df
daniela vera <daniela.vera@culturasoft.com>
2024-12-10 09:51:58 -0500

    operadores javascript

---
+++ operadores.js
@@ -0,0 +1,17 @@
+
+//OPERADORES
+const variable = 'valor de la variable' // = Operador de asignación 
+
+const suma = 1 + 3 // + Operador aritmetico de la suma
+const resta = 5 - 2 // - Operador aritmetico de la resta
+const multiplicación = 5 * 2 // * Operador aritmetico de la multiplicación 
+const division = 7 / 3 // / Operador aritmetico de la división 
+const resto = 4 % 2 // % Operador aritmetico del resto o módulo de una división
+const exponente = 2 ** 3 // ** Operador aritmetico de la potencia o exponente
+
+//INCREMENTO Y DECREMENTO
+let x = 5;
+let y = 10;
+
+x++; 
+y--; 
